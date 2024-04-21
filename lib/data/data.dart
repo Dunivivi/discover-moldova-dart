989156e2388b0ef21605b0d87f0801c618a189a8
Duca Tudor <duca.2001@mail.ru>
2024-04-21 21:20:09 +0300

    First commit. Home page mock & detail

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -6,72 +6,72 @@ List<CountryModel> getCountrys() {
   CountryModel countryModel = new CountryModel();
 
 //1
-  countryModel.countryName = "Thailand";
+  countryModel.countryName = "Orheiul Vechi";
   countryModel.label = "New";
   countryModel.noOfTours = 18;
   countryModel.rating = 4.5;
   countryModel.imgUrl =
-      "https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+      "https://photos.pandatur.md/e48e1c1fb92ff94d7b0ddb6e04937885.jpg";
   country.add(countryModel);
   countryModel = new CountryModel();
 
-  //1
-  countryModel.countryName = "Malaysia";
-  countryModel.label = "Sale";
+  //2
+  countryModel.countryName = "Dendrarium";
+  countryModel.label = "New";
   countryModel.noOfTours = 12;
   countryModel.rating = 4.3;
   countryModel.imgUrl =
-      "https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/409456790.jpg?k=a3a3ea0e836d5d0d015d4a251f1fedc080ec8976ee0fd064d2ffa3e98a2a9442&o=&hp=1";
   country.add(countryModel);
   countryModel = new CountryModel();
 
-  //1
-  countryModel.countryName = "Thailand";
+  //3
+  countryModel.countryName = "Valea Morilor";
   countryModel.label = "New";
   countryModel.noOfTours = 18;
   countryModel.rating = 4.5;
   countryModel.imgUrl =
-      "https://images.pexels.com/photos/3568489/pexels-photo-3568489.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+      "https://s.inyourpocket.com/img/figure/2020-02/chisinau_moldova_inyourpocket.jpg";
   country.add(countryModel);
   countryModel = new CountryModel();
 
-  //1
-  countryModel.countryName = "Thailand";
+  //4
+  countryModel.countryName = "Catedrala";
   countryModel.label = "New";
   countryModel.noOfTours = 18;
   countryModel.rating = 4.5;
   countryModel.imgUrl =
-      "https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Ansamblul_Catedralei_%E2%80%9ENa%C8%99terea_Domnului%E2%80%9D_8.jpg/1200px-Ansamblul_Catedralei_%E2%80%9ENa%C8%99terea_Domnului%E2%80%9D_8.jpg";
   country.add(countryModel);
   countryModel = new CountryModel();
 
-  //1
-  countryModel.countryName = "Thailand";
+  //5
+  countryModel.countryName = "Muzeul Național de Istorie";
   countryModel.label = "New";
   countryModel.noOfTours = 18;
   countryModel.rating = 4.5;
   countryModel.imgUrl =
-      "https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+      "https://visit.chisinau.md/wp-content/uploads/2021/09/21-2-1024x679.jpg";
   country.add(countryModel);
   countryModel = new CountryModel();
 
-  //1
-  countryModel.countryName = "Thailand";
+  //6
+  countryModel.countryName = "Asconi";
   countryModel.label = "New";
   countryModel.noOfTours = 18;
   countryModel.rating = 4.5;
   countryModel.imgUrl =
-      "https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+      "https://asconiwinery.com/wp-content/uploads/2021/03/asconi-winery-home-10-1.jpg";
   country.add(countryModel);
   countryModel = new CountryModel();
 
-  //1
-  countryModel.countryName = "Thailand";
+  //7
+  countryModel.countryName = "Cetatea Bender";
   countryModel.label = "New";
   countryModel.noOfTours = 18;
   countryModel.rating = 4.5;
   countryModel.imgUrl =
-      "https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/0f/fb/0d/1.jpg?w=1400&h=-1&s=1";
   country.add(countryModel);
   countryModel = new CountryModel();
 
@@ -84,45 +84,65 @@ List<PopularTourModel> getPopularTours() {
 
 //1
   popularTourModel.imgUrl =
-      "https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
-  popularTourModel.title = "Thailand";
-  popularTourModel.desc = "10 nights for two/all inclusive";
-  popularTourModel.price = "\$ 245.50";
-  popularTourModel.rating = 4.0;
-  popularTourModels.add(popularTourModel);
-  popularTourModel = new PopularTourModel();
-
-//1
-  popularTourModel.imgUrl =
-      "https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
-  popularTourModel.title = "Cuba";
-  popularTourModel.desc = "10 nights for two/all inclusive";
-  popularTourModel.price = "\$ 499.99";
+      "https://photos.pandatur.md/e48e1c1fb92ff94d7b0ddb6e04937885.jpg";
+  popularTourModel.title = "Orheiul Vechi";
+  popularTourModel.desc = "1 noapte pentru 2 persoane";
+  popularTourModel.price = "\$ 50";
   popularTourModel.rating = 4.5;
   popularTourModels.add(popularTourModel);
   popularTourModel = new PopularTourModel();
 
-//1
+//2
   popularTourModel.imgUrl =
-      "https://images.pexels.com/photos/1477430/pexels-photo-1477430.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
-  popularTourModel.title = "Dominican";
-  popularTourModel.desc = "10 nights for two/all inclusive";
-  popularTourModel.price = "\$ 245.50";
-  popularTourModel.rating = 4.2;
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/409456790.jpg?k=a3a3ea0e836d5d0d015d4a251f1fedc080ec8976ee0fd064d2ffa3e98a2a9442&o=&hp=1";
+  popularTourModel.title = "Dendrarium";
+  popularTourModel.desc = "4 persoane";
+  popularTourModel.price = "\$ 5";
+  popularTourModel.rating = 4.3;
   popularTourModels.add(popularTourModel);
   popularTourModel = new PopularTourModel();
 
-//1
+//3
   popularTourModel.imgUrl =
-      "https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
-  popularTourModel.title = "Thailand";
-  popularTourModel.desc = "10 nights for two/all inclusive";
-  popularTourModel.price = "\$ 245.50";
-  popularTourModel.rating = 4.0;
+      "https://asconiwinery.com/wp-content/uploads/2021/03/asconi-winery-home-10-1.jpg";
+  popularTourModel.title = "Asconi";
+  popularTourModel.desc = "Vinăria ce reprezintă Republica Moldova";
+  popularTourModel.price = "\$ 100";
+  popularTourModel.rating = 4.5;
   popularTourModels.add(popularTourModel);
   popularTourModel = new PopularTourModel();
 
-//1
+//4
+  popularTourModel.imgUrl =
+      "https://visit.chisinau.md/wp-content/uploads/2021/09/21-2-1024x679.jpg";
+  popularTourModel.title = "Muzeul Național de Istorie";
+  popularTourModel.desc = "Istoria țării într-un edificiu";
+  popularTourModel.price = "\$ 10";
+  popularTourModel.rating = 4.5;
+  popularTourModels.add(popularTourModel);
+  popularTourModel = new PopularTourModel();
+
+//5
+  popularTourModel.imgUrl =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Ansamblul_Catedralei_%E2%80%9ENa%C8%99terea_Domnului%E2%80%9D_8.jpg/1200px-Ansamblul_Catedralei_%E2%80%9ENa%C8%99terea_Domnului%E2%80%9D_8.jpg";
+  popularTourModel.title = "Catedrala";
+  popularTourModel.desc = "Simbolul creștinismului";
+  popularTourModel.price = "\$ 0";
+  popularTourModel.rating = 4.5;
+  popularTourModels.add(popularTourModel);
+  popularTourModel = new PopularTourModel();
+
+//6
+  popularTourModel.imgUrl =
+      "https://s.inyourpocket.com/img/figure/2020-02/chisinau_moldova_inyourpocket.jpg";
+  popularTourModel.title = "Valea Morilor";
+  popularTourModel.desc = "Parc pentru relaxare";
+  popularTourModel.price = "\$ 0";
+  popularTourModel.rating = 4.5;
+  popularTourModels.add(popularTourModel);
+  popularTourModel = new PopularTourModel();
+
+//7
   popularTourModel.imgUrl =
       "https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   popularTourModel.title = "Thailand";
@@ -132,27 +152,7 @@ List<PopularTourModel> getPopularTours() {
   popularTourModels.add(popularTourModel);
   popularTourModel = new PopularTourModel();
 
-//1
-  popularTourModel.imgUrl =
-      "https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
-  popularTourModel.title = "Thailand";
-  popularTourModel.desc = "10 nights for two/all inclusive";
-  popularTourModel.price = "\$ 245.50";
-  popularTourModel.rating = 4.0;
-  popularTourModels.add(popularTourModel);
-  popularTourModel = new PopularTourModel();
-
-//1
-  popularTourModel.imgUrl =
-      "https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
-  popularTourModel.title = "Thailand";
-  popularTourModel.desc = "10 nights for two/all inclusive";
-  popularTourModel.price = "\$ 245.50";
-  popularTourModel.rating = 4.0;
-  popularTourModels.add(popularTourModel);
-  popularTourModel = new PopularTourModel();
-
-//1
+//8
   popularTourModel.imgUrl =
       "https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
   popularTourModel.title = "Thailand";
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -13,6 +13,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<PopularTourModel> popularTourModels = new List();
   List<CountryModel> country = new List();
+
   @override
   void initState() {
     country = getCountrys();
@@ -20,41 +21,49 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
+  // Track current route
+  String currentRoute = "home";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Container(
-          padding: EdgeInsets.all(7),
-          child: Image.asset(
-            "assets/menu.png",
-            height: 20,
-            width: 20,
-          ),
-        ),
+        backgroundColor: Color(0xfffefefe),
+        // leading: Container(
+        //   padding: EdgeInsets.all(7),
+        //   child: Image.asset(
+        //     "assets/menu.png",
+        //     height: 20,
+        //     width: 20,
+        //   ),
+        // ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              "assets/logo.png",
-              height: 30,
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              // Adjust the value as needed
+              child: Image.asset(
+                "assets/logo.png",
+                height: 30,
+              ),
             ),
             Text(
-              "DiscountTour",
+              "Descoperă Moldova",
               style:
-                  TextStyle(color: Colors.black54, fontWeight: FontWeight.w600),
+              TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
             )
           ],
         ),
-        actions: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Icon(
-              Icons.more_vert,
-              color: Colors.white,
-            ),
-          )
-        ],
+        // actions: [
+        //   Container(
+        //     padding: EdgeInsets.symmetric(horizontal: 16),
+        //     child: Icon(
+        //       Icons.more_vert,
+        //       color: Colors.black,
+        //     ),
+        //   )
+        // ],
         elevation: 0.0,
       ),
       body: SingleChildScrollView(
@@ -64,7 +73,7 @@ class _HomeState extends State<Home> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "Find the best tour",
+                "Pentru tine",
                 style: TextStyle(
                     fontSize: 28,
                     color: Colors.black54,
@@ -73,16 +82,16 @@ class _HomeState extends State<Home> {
               SizedBox(
                 height: 8,
               ),
-              Text(
-                "Country",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black54,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 16,
-              ),
+              // Text(
+              //   "Country",
+              //   style: TextStyle(
+              //       fontSize: 20,
+              //       color: Colors.black54,
+              //       fontWeight: FontWeight.w600),
+              // ),
+              // SizedBox(
+              //   height: 16,
+              // ),
               Container(
                 height: 240,
                 child: ListView.builder(
@@ -104,7 +113,7 @@ class _HomeState extends State<Home> {
                 height: 8,
               ),
               Text(
-                "Popular Tours",
+                "Categorii",
                 style: TextStyle(
                     fontSize: 20,
                     color: Colors.black54,
@@ -113,7 +122,9 @@ class _HomeState extends State<Home> {
               SizedBox(
                 height: 16,
               ),
+              CategoryList(),
               ListView.builder(
+                  padding: EdgeInsets.only(top: 10),
                   shrinkWrap: true,
                   physics: ClampingScrollPhysics(),
                   itemCount: popularTourModels.length,
@@ -130,6 +141,168 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
+      bottomNavigationBar: Container(
+        color: Color(0xfffefefe),
+        // Background color
+        padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
+        // Padding for the container, excluding top
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildNavItem(
+              icon: Icons.explore,
+              text: 'Explorează',
+              isActive: currentRoute == "home",
+              onPressed: () {
+                // Explore button action
+                setState(() {
+                  currentRoute = "home";
+                });
+              },
+            ),
+            buildNavItem(
+              icon: Icons.favorite,
+              text: 'Favorite',
+              isActive: currentRoute == "favorites",
+              onPressed: () {
+                // Favorites button action
+                setState(() {
+                  currentRoute = "favorites";
+                });
+              },
+            ),
+            buildNavItem(
+              icon: Icons.event,
+              text: 'Evenimente',
+              isActive: currentRoute == "events",
+              onPressed: () {
+                // Events button action
+                setState(() {
+                  currentRoute = "events";
+                });
+              },
+            ),
+            buildNavItem(
+              icon: Icons.account_circle,
+              text: 'Profil',
+              isActive: currentRoute == "profile",
+              onPressed: () {
+                // Profile button action
+                setState(() {
+                  currentRoute = "profile";
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildNavItem(
+      {IconData icon, String text, bool isActive, VoidCallback onPressed}) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: Icon(
+            icon,
+            color: isActive
+                ? Colors.blue
+                : Colors.black, // Change color based on isActive
+          ),
+          onPressed: onPressed,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 12.0,
+            color: isActive
+                ? Colors.blue
+                : Colors.black, // Change color based on isActive
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CategoryList extends StatefulWidget {
+  @override
+  _CategoryListState createState() => _CategoryListState();
+}
+
+class _CategoryListState extends State<CategoryList> {
+  String selectedCategory;
+
+  final List<String> categories = [
+    "Toate",
+    "Relaxare",
+    "Plimbare",
+    "Istorie",
+    "Cazare"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    selectedCategory = categories.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: categories
+            .map((category) =>
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              // Adjust horizontal spacing between categories
+              child: CategoryButton(
+                category: category,
+                isSelected: category == selectedCategory,
+                onPressed: () {
+                  setState(() {
+                    selectedCategory = category;
+                  });
+                },
+              ),
+            ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class CategoryButton extends StatelessWidget {
+  final String category;
+  final bool isSelected;
+  final VoidCallback onPressed;
+
+  CategoryButton({this.category, this.isSelected, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isSelected ? Color(0xff454545) : Color(0xfffefefe),
+          // Background color
+          borderRadius: BorderRadius.circular(15.0), // Border radius
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        // Adjust padding as needed
+        child: Center(
+          child: Text(
+            category,
+            style: TextStyle(
+              color: isSelected ? Colors.white : Colors.black, // Text color
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -140,12 +313,12 @@ class PopularTours extends StatelessWidget {
   final String desc;
   final String price;
   final double rating;
-  PopularTours(
-      {@required this.imgUrl,
-      @required this.rating,
-      @required this.desc,
-      @required this.price,
-      @required this.title});
+
+  PopularTours({@required this.imgUrl,
+    @required this.rating,
+    @required this.desc,
+    @required this.price,
+    @required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -154,7 +327,8 @@ class PopularTours extends StatelessWidget {
         Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) => Details(
+                builder: (context) =>
+                    Details(
                       imgUrl: imgUrl,
                       placeName: title,
                       rating: rating,
@@ -178,44 +352,49 @@ class PopularTours extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff4E6059)),
-                  ),
-                  SizedBox(
-                    height: 3,
-                  ),
-                  Text(
-                    desc,
-                    style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xff89A097)),
-                  ),
-                  SizedBox(
-                    height: 6,
-                  ),
-                  Text(
-                    price,
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff4E6059)),
-                  )
-                ],
+            Expanded( // To make the first container take 80% of the width
+              flex: 8, // 8
+              child: // 0% of available
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff4E6059)),
+                    ),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      desc,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff89A097)),
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      price,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff4E6059)),
+                    )
+                  ],
+                ),
               ),
             ),
             Container(
                 margin: EdgeInsets.only(bottom: 10, right: 8),
-                padding: EdgeInsets.symmetric(horizontal: 4, vertical: 12),
+                padding: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(6),
                     color: Color(0xff139157)),
@@ -251,12 +430,12 @@ class CountryListTile extends StatelessWidget {
   final int noOfTours;
   final double rating;
   final String imgUrl;
-  CountryListTile(
-      {@required this.countryName,
-      @required this.label,
-      @required this.noOfTours,
-      @required this.rating,
-      @required this.imgUrl});
+
+  CountryListTile({@required this.countryName,
+    @required this.label,
+    @required this.noOfTours,
+    @required this.rating,
+    @required this.imgUrl});
 
   @override
   Widget build(BuildContext context) {
@@ -283,7 +462,7 @@ class CountryListTile extends StatelessWidget {
                     Container(
                         margin: EdgeInsets.only(left: 8, top: 8),
                         padding:
-                            EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+                        EdgeInsets.symmetric(vertical: 6, horizontal: 8),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(8),
                             color: Colors.white38),
@@ -296,60 +475,62 @@ class CountryListTile extends StatelessWidget {
                 Spacer(),
                 Row(
                   children: [
-                    Container(
-                      margin: EdgeInsets.only(bottom: 10, left: 8, right: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            child: Text(
-                              "Thailand",
-                              style: TextStyle(
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            bottom: 0, left: 8, right: 8, top: 40),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              child: Text(
+                                countryName,
+                                style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.w600,
-                                  fontSize: 16),
+                                  fontSize: 16,
+                                ),
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 3,
-                          ),
-                          Text(
-                            "18 Tours",
-                            style: TextStyle(
+                            SizedBox(height: 3),
+                            Text(
+                              noOfTours.toString() + " vizite recente",
+                              style: TextStyle(
                                 color: Colors.white,
                                 fontWeight: FontWeight.w600,
-                                fontSize: 13),
+                                fontSize: 13,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 0, right: 5, top: 30),
+                      padding: EdgeInsets.symmetric(horizontal: 3, vertical: 7),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        color: Colors.white38,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            rating.toString(),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 13,
+                            ),
+                          ),
+                          SizedBox(height: 2),
+                          Icon(
+                            Icons.star,
+                            color: Colors.white,
+                            size: 20,
                           )
                         ],
                       ),
-                    ),
-                    Spacer(),
-                    Container(
-                        margin: EdgeInsets.only(bottom: 10, right: 8),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 3, vertical: 7),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(3),
-                            color: Colors.white38),
-                        child: Column(
-                          children: [
-                            Text(
-                              "4.5",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 13),
-                            ),
-                            SizedBox(
-                              height: 2,
-                            ),
-                            Icon(
-                              Icons.star,
-                              color: Colors.white,
-                              size: 20,
-                            )
-                          ],
-                        ))
+                    )
                   ],
                 )
               ],
